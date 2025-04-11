e5eb47067154d8342d4dfecd726c0fca8e3d9816
MahadeHasan <mahadehasan421@gmail.com>
2025-04-11 14:21:37 +0600

    updated files

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 17151509
+let accountEmail = "mahadehasan421@gmail.com"
+var accountPassword = "12345"
+accountCity = "Dhaka"
+
+//accountId = 2
+  
+accountEmail = "mahade2gmail.com"
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+
